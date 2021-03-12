ea26f4938c5a3a32978b51901fa2432c634f3424
WuBoytH <alexwuief@gmail.com>
2021-03-11 20:12:00 -0500

    Made Prospector Solitaire

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class Card : MonoBehaviour {
 
+	public bool isGold = false;
 	public string    suit;
 	public int       rank;
 	public Color     color = Color.black;
@@ -15,6 +16,7 @@ public class Card : MonoBehaviour {
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
 
+	public SpriteRenderer[] spriteRenderers;
 
 	public bool faceUp {
 		get {
@@ -26,15 +28,65 @@ public class Card : MonoBehaviour {
 		}
 	}
 
+	virtual public void OnMouseUpAsButton() {
+		print(name);
+	}
+
 
 	// Use this for initialization
 	void Start () {
-	
+		SetSortOrder(0);
+		// if (isGold) {
+		// 	foreach (var SpriteRenderer in spriteRenderers) {
+		// 		if (SpriteRenderer.gameObject.name == "face") {
+		// 			GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Cards/Card_Front_Gold");
+		// 			spriteRenderer.gameObject.AddComponent<SetSortOrder>();
+		// 		}
+		// 		if (SpriteRenderer.gameObject.name == "back") {
+		// 			GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Cards/Card_Back_Gold");
+		// 		}
+		// 	}
+		// }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void PopulateSpriteRenderers() {
+		if (spriteRenderers == null || spriteRenderers.Length == 0) {
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+
+	public void SetSortingLayerName(string tSLN) {
+		PopulateSpriteRenderers();
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	public void SetSortOrder(int sOrd) {
+		PopulateSpriteRenderers();
+
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			if (tSR.gameObject == this.gameObject) {
+				tSR.sortingOrder = sOrd;
+				continue;
+			}
+
+			switch (tSR.gameObject.name) {
+				case "back":
+					tSR.sortingOrder = sOrd + 2;
+					break;
+				
+				case "face":
+				default:
+					tSR.sortingOrder = sOrd + 1;
+					break;
+			}
+		}
 	}
 } // class Card
 
