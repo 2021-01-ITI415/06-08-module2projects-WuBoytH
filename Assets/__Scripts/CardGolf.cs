9764180c66a1bf8d550b407ed804034840b4ca7a
WuBoytH <alexwuief@gmail.com>
2021-03-31 13:44:32 -0400

    First Attempt at changing the layout

---
+++ Assets/__Scripts/CardGolf.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eCardStateGolf {
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+
+public class CardGolf : Card
+{
+
+    [Header("Set Dynamically: CardGolf")]
+    public eCardStateGolf state = eCardStateGolf.drawpile;
+    public List<CardGolf> hiddenBy = new List<CardGolf>();
+    public int layoutID;
+    public SlotDefGolf slotDefGolf;
+
+    override public void OnMouseUpAsButton() {
+        Golf.S.CardClicked(this);
+        base.OnMouseUpAsButton();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
